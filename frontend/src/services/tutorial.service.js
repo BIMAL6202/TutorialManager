855d38fdbf8a6677dd6a7ba56cb7b90995831cc4
BIMAL6202 <bimalraj6202@gmail.com>
2025-07-21 20:58:51 +0530

    Frontend env variable created

--- frontend/src/services/tutorial.service.js
+++ frontend/src/services/tutorial.service.js
@@ -1,40 +1,44 @@
 import axios from "axios";
- 
+
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
 const http = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: API_BASE_URL,  // Use env variable here
     headers: {
         "Content-Type": "application/json",
     },
 });
- 
+
+// rest of your functions remain same...
+
 const getAll = () => {
     return http.get("/tutorials");
 };
- 
+
 const get = (id) => {
     return http.get(`/tutorials/${id}`);
 };
- 
+
 const create = (data) => {
     return http.post("/tutorials", data);
 };
- 
+
 const update = (id, data) => {
     return http.put(`/tutorials/${id}`, data);
 };
- 
+
 const remove = (id) => {
     return http.delete(`/tutorials/${id}`);
 };
- 
+
 const removeAll = () => {
     return http.delete("/tutorials");
 };
- 
+
 const findByTitle = (title) => {
     return http.get(`/tutorials?title=${title}`);
 };
- 
+
 export default {
     getAll,
     get,
